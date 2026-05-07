3ea464f4d64c1c329724ff67ce497eb30ff433ff
Manu <manulopezayora1985@protonmail.com>
2026-05-07 12:08:06 +0200

    chore(round-selector): implement accessibility

--- src/components/RoundSelector/RoundSelector.tsx
+++ src/components/RoundSelector/RoundSelector.tsx
@@ -10,19 +10,20 @@ export const RoundSelector = ({ bestOf, setBestOf }: RoundSelectorProps) => {
     const options: RoundOption[] = [1, 3, 5];
 
     return (
-        <div className={styles.round_selector}>
+        <fieldset className={styles.round_selector}>
+            <legend className={styles.legend}>Number of rounds</legend>
             {options.map((option) => (
-                <label key={option} style={{ cursor: "pointer", fontSize: 20, fontWeight: '500' }}>
+                <label key={option} className={styles.label}>
                     <input
                         type="radio"
+                        name="best-of"
                         value={option}
                         checked={bestOf === option}
                         onChange={() => setBestOf(option)}
-                        style={{ marginRight: 6 }}
                     />
                     {`Best of ${option}`}
                 </label>
             ))}
-        </div>
+        </fieldset>
     )
 }
